a5f7f4ce0847189e7a573f29f2a64acb7399764e
zhangxiang-plct <zhangxiang@iscas.ac.cn>
2022-11-07 21:50:11 +0800

    fix MethodHandles::jump_to_lambda_form

--- hotspot/src/cpu/riscv64/vm/methodHandles_riscv64.cpp
+++ hotspot/src/cpu/riscv64/vm/methodHandles_riscv64.cpp
@@ -142,9 +142,9 @@ void MethodHandles::jump_to_lambda_form(MacroAssembler* _masm,
   __ load_heap_oop_rv(method_temp, Address(method_temp, NONZERO(java_lang_invoke_LambdaForm::vmentry_offset_in_bytes())));
   __ verify_oop(method_temp);
   __ load_heap_oop_rv(method_temp, Address(method_temp, NONZERO(java_lang_invoke_MemberName::method_offset_in_bytes())));
-  __ verify_oop(method_temp);
+  //__ verify_oop(method_temp);
  // __ access_load_at(T_ADDRESS, IN_HEAP, method_temp, Address(method_temp, NONZERO(java_lang_invoke_ResolvedMethodName::vmtarget_offset_in_bytes())), noreg, noreg);
-  __ ld(method_temp, Address(method_temp, NONZERO(java_lang_invoke_MemberName::vmtarget_offset_in_bytes())));
+ // __ ld(method_temp, Address(method_temp, NONZERO(java_lang_invoke_MemberName::vmtarget_offset_in_bytes())));
   if (VerifyMethodHandles && !for_compiler_entry) {
     // make sure recv is already on stack
     __ ld(temp2, Address(method_temp, Method::const_offset()));
@@ -285,7 +285,7 @@ void MethodHandles::generate_method_handle_dispatch(MacroAssembler* _masm,
     Address member_clazz(    member_reg, NONZERO(java_lang_invoke_MemberName::clazz_offset_in_bytes()));
     Address member_vmindex(  member_reg, NONZERO(java_lang_invoke_MemberName::vmindex_offset_in_bytes()));
     Address member_vmtarget( member_reg, NONZERO(java_lang_invoke_MemberName::method_offset_in_bytes()));
-    Address vmtarget_method( xmethod, NONZERO(java_lang_invoke_ResolvedMethodName::vmtarget_offset_in_bytes()));
+    //Address vmtarget_method( xmethod, NONZERO(java_lang_invoke_ResolvedMethodName::vmtarget_offset_in_bytes()));
 
     Register temp1_recv_klass = temp1;
     if (iid != vmIntrinsics::_linkToStatic) {
