da457d4d684ca610cd76900b0351b6fabcf7b46d
zhangxiang-plct <zhangxiang@iscas.ac.cn>
2022-11-23 16:18:36 +0800

    fix adGlobals_riscv64.hpp in regmask.hpp

--- hotspot/src/share/vm/opto/regmask.hpp
+++ hotspot/src/share/vm/opto/regmask.hpp
@@ -43,7 +43,7 @@
 #elif defined TARGET_ARCH_MODEL_ppc_64
 # include "adfiles/adGlobals_ppc_64.hpp"
 #elif defined TARGET_ARCH_MODEL_riscv64
-# include "adfiles/ad_riscv64.hpp"
+# include "adfiles/adGlobals_riscv64.hpp"
 #endif
 
 // Some fun naming (textual) substitutions:
